a8e83da88a1a48b195c1931792b62cf3638b1837
poohitan <poohitan@gmail.com>
2018-01-30 12:48:16 +0200

    Optimize Header component for mobile screens

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Logo from './ui/Logo';
 
 class Header extends React.Component {
-  componentDidMount() {
+  static initHeadroom() {
     const { Headroom } = window;
 
     if (!Headroom) {
@@ -19,17 +19,48 @@ class Header extends React.Component {
     headroom.init();
   }
 
+  static initHamburgerButton() {
+    const $menu = $('.header .menu');
+
+    $('.header .menu-hamburger').on('click', (event) => {
+      event.stopPropagation();
+
+      if ($menu.hasClass('menu-visible')) {
+        $menu.removeClass('menu-visible');
+      } else {
+        $menu.addClass('menu-visible');
+      }
+    });
+
+    $('body').on('click', () => {
+      $menu.removeClass('menu-visible');
+    });
+  }
+
+  componentDidMount() {
+    Header.initHeadroom();
+    Header.initHamburgerButton();
+  }
+
   render() {
     return (
       <section className="header">
         <Link href="/"><a><Logo /></a></Link>
-        <nav>
+        <nav className="layout-row layout-align-start-center">
+          <div className="search-button">
+            <Link href="/search">
+              <a>
+                <i className="fa fa-search" /><span className="sr-only">Facebook</span>
+              </a>
+            </Link>
+          </div>
           <ul className="menu">
             <li><Link href="/projects"><a>Проекти</a></Link></li>
             <li><a>Ниття</a></li>
             <li><a>Ше шось</a></li>
             <li><Link href="/page?path=about" as="/about"><a>Про</a></Link></li>
           </ul>
+          <i className="menu-hamburger fa fa-bars" />
         </nav>
       </section>
     );
